839e4ebb5442290c6e68d2012dbd53fe5960770f
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-02-21 18:09:14 -0500

    lab done

---
+++ src/components/IdCard.js
@@ -0,0 +1,18 @@
+
+const IdCard = (props) => {
+    const date = String(props.birth)
+    return (
+        <div id ="card">
+            <img src= {props.picture} alt="profile"/>
+            <div>
+                <p><span className="card-span">First name:</span> {props.firstName}</p>
+                <p><span className="card-span">Last name:</span>{props.lastName}</p>
+                <p><span className="card-span">Gender:</span>{props.gender}</p>
+                <p><span className="card-span">Height:</span>{props.height * 0.01}</p>
+                <p><span className="card-span">Birth:</span>{date}</p>
+            </div>
+        </div>
+    )
+}
+
+export default IdCard
